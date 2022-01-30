3dece6c7a4f60fbb3e7fafc75ec798beab96e5ad
Andreas Christianson <achristianson@pessimistic-it.com>
2022-01-30 14:54:07 -0600

    nitial commit from https://start.spring.io/#!type=maven-project&language=kotlin&platformVersion=2.6.3&packaging=jar&jvmVersion=17&groupId=com.pessimistic-it&artifactId=scarcity-backend&name=scarcity-backend&description=&packageName=com.pessimistic-it.scarcity-backend&dependencies=flyway,data-jpa,postgresql,devtools,data-rest,actuator

---
+++ src/main/kotlin/com/pessimisticit/scarcitybackend/ScarcityBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.pessimisticit.scarcitybackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ScarcityBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<ScarcityBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/pessimisticit/scarcitybackend/ScarcityBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pessimisticit.scarcitybackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ScarcityBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
