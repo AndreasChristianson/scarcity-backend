95c755892cb3305fcfa497d0ae9d45dd061f7f93
Andreas Christianson <achristianson@pessimistic-it.com>
2022-01-31 09:12:37 -0600

    data sources configured

---
+++ src/main/kotlin/com/pessimisticit/scarcitybackend/configuration/DataSource.kt
@@ -0,0 +1,34 @@
+
+package com.pessimisticit.scarcitybackend.configuration;
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.jdbc.DataSourceBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.stereotype.Component
+import org.springframework.web.util.UriComponentsBuilder
+import java.net.URI
+import java.net.URL
+import javax.sql.DataSource
+
+
+@Configuration
+class DataSourceConfig {
+
+  @Bean
+  @ConfigurationProperties(prefix = "app.datasource")
+  fun dataSource(@Value("\${app.datasource.url}") connectionString: String): DataSource {
+    val dsb = DataSourceBuilder.create() ?: throw Exception("Unable to init data source builder")
+    val parsedConnectionString = URI(connectionString)
+    val (user, password) = parsedConnectionString.userInfo.split(":")
+
+    val urlString = "jdbc:postgresql://${parsedConnectionString.host}:${parsedConnectionString.port}${parsedConnectionString.path}"
+
+    dsb.username(user).password(password).url(urlString)
+
+    return dsb.build()
+  }
+
+}
---
+++ src/test/kotlin/com/pessimisticit/scarcitybackend/configuration/TestDataSource.kt
@@ -0,0 +1,24 @@
+package com.pessimisticit.scarcitybackend.configuration
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.jdbc.datasource.DriverManagerDataSource
+
+
+@Configuration
+class TestDataSource {
+
+
+		@Bean
+		@Primary
+		fun embeddedDataSource(): DriverManagerDataSource {
+				val driverManagerDataSource = DriverManagerDataSource()
+				driverManagerDataSource.setDriverClassName("org.h2.Driver")
+				driverManagerDataSource.url = "jdbc:h2:mem:testdb;IGNORECASE=TRUE;DB_CLOSE_ON_EXIT=FALSE;DB_CLOSE_DELAY=-1"
+				return driverManagerDataSource
+			}
+
+}
